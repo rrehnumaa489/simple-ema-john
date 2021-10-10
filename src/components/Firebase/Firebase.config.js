099542bfe0f854a4a0afc734f144ef31b9e71939
rehnuma tasnim <rehnumamorshedbd@gmail.com>
2021-10-10 22:35:01 +0600

    first commit

---
+++ src/components/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSqpw6QJIYRHG2bu9KioQx59Y_roHJx1A",
+    authDomain: "simple-ema-john-c0367.firebaseapp.com",
+    projectId: "simple-ema-john-c0367",
+    storageBucket: "simple-ema-john-c0367.appspot.com",
+    messagingSenderId: "532123755748",
+    appId: "1:532123755748:web:7716062254d9acddebf8f1"
+  };
+
+  export default firebaseConfig;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className="login-form">
+            <div>
+                <h2>Login</h2>
+                <form onSubmit="">
+                    <input type="email" name="" id="" placeholder="Your Email" /><br />
+                    <input type="password" name="" id="" /><br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>New to ema-john? <Link to="/register">Create Account</Link></p>
+                <div>........or........</div> <br />
+                <button className="btn-regular">Google Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
